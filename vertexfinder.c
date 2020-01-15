6a3f030e3f242b9aad6c8f3604c361fde8474df3
miaCx <33041196+miaCx@users.noreply.github.com>
2020-01-15 13:42:52 -0800

    Update vertexfinder.c

--- vertexfinder.c
+++ vertexfinder.c
@@ -12,30 +12,50 @@ float findVY(float a, float b, float c, float vx) {
   return vy;
 }
 
-void printvform(float a, float c,float vx,float vy) {
-  if(vx < 0) {
-    if (a == 1) {
-      if (vx > 0){
-        printf("The vertex form is (x-%f)+%f\n",vx,vy);
-      }
-      else {
-        printf("The vertex form is (x%f)+%f\n",vx,vy);
-      }
+void printvform(float a,float vx,float vy) {
+  //printf("The vertex form is %f(x-%f)+%f\n",a,vx,vy);
+  printf("The vertex form is ");
+  if (a != 1) {
+      printf("%f",a);  
+      printf("(x");
+    if (vx > 0) {
+      printf("-%f",vx);
     }
-    else if (vy == 0){
-      printf("The vertex form is (x-%f)\n",vx);
+    else if (vx < 0) {
+      float tmp = vx * -1;      
+      printf("+%f",tmp);
+    }
+    printf(")^2");
+    if (vy > 0) {
+      printf("+%f\n",vy);
+    }
+    else if (vy < 0) {
+      printf("%f\n",vy);
     }
     else {
-      printf("The vertex form is %f(x%f)+%f\n",a,vx,vy);
+      printf("\n");
     }
-
-  }
-  else if (vx == 0) {
-    printf("The vertex form is %fx+%f\n",a,vy);
   }
   else {
-    printf("The vertex form is %f(x-%f)+%f\n",a,vx,vy);
+    float tmp = vx + vy;
+    if (tmp > 0) {
+      printf("x^2+%f\n",tmp);
+    }
+    else if (tmp < 0) {
+      printf("x^2%f\n",tmp);
+    }
+    else {
+      printf("x^2\n");
+    }
+    //printf("");
   }
+}
+
+void doall (float a, float b, float c) {
+  float vx = findVX(a,b);
+  float vy = findVY(a,b,c,vx);
+  printf("The vertex is (%f,%f)\n", vx, vy);
+  printvform(a,vx,vy);
 }
 
 int main (int argc, char* argv[]) {
@@ -48,14 +68,13 @@ int main (int argc, char* argv[]) {
       printf("Please enter a valid number for each argument.\n");
       return 0;
     }
-
-    float vx = findVX(a,b);
-    float vy = findVY(a,b,c,vx);
-    printf("The vertex is (%f,%f)\n", vx, vy);
-    printvform(a,c,vx,vy);
+    
+    doall(a,b,c);
+    return 0;
     
   }
   else {
     printf("Please enter a valid number for each argument.\n");
+    return 0;
   }
 }
